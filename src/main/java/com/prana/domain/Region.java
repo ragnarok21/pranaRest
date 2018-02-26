f8ba073a1b3c489db9db65c4c6dfbf8b28598e1c
Ramon Duran <ramon.duran@alaya.cl>
2018-02-26 11:33:56 -0300

    se remueve version de region

--- src/main/java/com/prana/domain/Region.java
+++ src/main/java/com/prana/domain/Region.java
@@ -19,8 +19,6 @@ public class Region implements Serializable{
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "region_id")
     private Integer id;
-    @Version
-    private Integer version;
     @Column(unique = true)
     private String nombre;
 }
