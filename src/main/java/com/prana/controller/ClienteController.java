5f50059dc209338631fbfd75e29f5810856d268b
Ramon Duran <ramon.duran@alaya.cl>
2018-02-28 17:58:55 -0300

    se agrega jwt

--- src/main/java/com/prana/controller/ClienteController.java
+++ src/main/java/com/prana/controller/ClienteController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/cliente")
+@RequestMapping("/clientes")
 @CrossOrigin(origins = "http://localhost:7000")
 public class ClienteController {
 
